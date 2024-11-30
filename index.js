0f7149fa38f2d0bf4d4e4204386868f7afc66fa6
AYZBTR <bhattaraiaayush.78@gmail.com>
2024-11-30 20:42:31 +0200

    fixed wrapping password in quotes because database password contains special character:)

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const mysql = require('mysql2');
 // Load environment variables from .env file
 require('dotenv').config();
 
-/* // Create the connection to database
- const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'Node_with_SQL',
-    password: '@#mysql@#'
-});  */
 // Create the connection to the database using environment variables
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
