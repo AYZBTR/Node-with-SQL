678b66c707d8ca2e6ab6ec6c6e08e71b475c2aee
AYZBTR <bhattaraiaayush.78@gmail.com>
2024-11-30 20:38:22 +0200

    added .env file

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const { faker } = require('@faker-js/faker'); 
 const mysql = require('mysql2');
+// Load environment variables from .env file
+require('dotenv').config();
 
-
-// Create the connection to database
+/* // Create the connection to database
  const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     database: 'Node_with_SQL',
     password: '@#mysql@#'
-}); 
-
+});  */
+// Create the connection to the database using environment variables
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+});
 
 // A simple query
  try {
