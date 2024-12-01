8bbdd3f35f3f6713d4707f92b56c40a0c8216a9b
AYZBTR <bhattaraiaayush.78@gmail.com>
2024-12-02 00:47:35 +0200

    user can update new username using password and new user can also be added also user can delete their identification!

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const mysql = require('mysql2');
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
+const { v4: uuidv4 } = require("uuid");
 // Load environment variables from .env file
 require('dotenv').config();
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "/views"));
 
@@ -25,52 +29,32 @@ let getRandomUser = () => {
   ];
 };
 
-// A simple query
-// Inserting new data
-/* let query = "INSERT INTO user (id, username, email, password) VALUES ?";
-
-let data = [];
-for(let i=1; i<=100; i++){
-  data.push(getRandomUser()); //100 fake users...
-} */
-
-/*   try {
-    connection.query(query, [data], (err, result) => {
-      if (err) throw err;
-      console.log(result);
-    });
-  } catch (err) {
-    console.log(err);
-  } */
-  
-
-/* connection.end();   */
 
 //Home ROUTE.....
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
   let q = "SELECT count(*) FROM user";
   try {
     connection.query(q, (err, result) => {
       if (err) throw err;
       let count = result[0]["count(*)"];
-      res.render("home.ejs", {count});
+      res.render("home.ejs", { count });
     });
   } catch (err) {
     console.log(err);
     res.send("Database error!")
   }
-  
+
 });
 
 //GET /user --> Fetch and show (userid, username, email) of all users!
-app.get("/user", (req, res)=>{
+app.get("/user", (req, res) => {
   let q = "SELECT * FROM user";
   try {
-    connection.query(q, (err, users)=>{
+    connection.query(q, (err, users) => {
       if (err) throw err;
-      res.render("showusers.ejs", {users})
+      res.render("showusers.ejs", { users })
     });
-  }catch (err){
+  } catch (err) {
     console.log(err);
     res.send("Database error!")
   }
@@ -81,28 +65,130 @@ app.get("/user", (req, res)=>{
 //1. GET /user/:id/edit --> To get form to edit the username, based on id. This form will require password.
 // 2. PATCH /user/:id  --> To edit username, if correct password was entered in form.
 
-//Edit route:
-app.get("/user/:id/edit", (req,res)=>{
-  let {id} = req.params;
+//1. Edit route:
+app.get("/user/:id/edit", (req, res) => {
+  let { id } = req.params;
   let q = `SELECT * FROM user WHERE id= '${id}'`;
   try {
-    connection.query(q, (err, result)=>{
+    connection.query(q, (err, result) => {
       if (err) throw err;
       let user = result[0];
       res.render("edit.ejs", { user });
     });
-  }catch (err){
+  } catch (err) {
     console.log(err);
     res.send("Database error!")
   }
-  
+
 });
 
-//PATCH Request
+//2. PATCH Request (UPDATE ROUTE (DB))
+app.patch("/user/:id", (req, res) => {
+  let { id } = req.params;
+  let { password: formPass, username: newUsername } = req.body;
+  let q = `SELECT * FROM user WHERE id= '${id}'`;
 
 
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+      if (formPass != user.password) {
+        res.send("Incorrect Password! Try again!");
+      } else {
+        let q2 = `UPDATE user SET username='${newUsername}' WHERE id='${id}'`;
+        connection.query(q2, (err, result) => {
+          if (err) throw err;
+          res.redirect("/user");
+        });
+      }
 
-app.listen("8080", ()=>{
+    });
+  } catch (err) {
+    console.log(err);
+    res.send("Database error!")
+  }
+});
+//Explanation of this patch request/update route.
+/* When a PATCH request comes to our app, it will include the user ID, password, and the new username.
+
+First, we will fetch the user’s information from the database. Then, we’ll check whether the password provided in the form matches the password stored in the database.
+ • If the passwords don’t match, we’ll send a “Wrong Password” response.
+ • If they do match, there’s no problem. In that case, we simply update the username.
+
+Next, we’ll run the query to update the username.
+ • If an error occurs during the query execution, we’ll throw an error.
+ • If the query executes successfully and we get the result, we’ll send that result back in the response. */
+
+//Route to add new user to database
+app.get("/user/new", (req, res) => {
+  res.render("new.ejs");
+});
+app.post("/user/new", (req, res) => {
+  let { username, email, password } = req.body;
+  let id = uuidv4();
+  //Query to Insert New User
+  let q = `INSERT INTO user (id, username, email, password) VALUES ('${id}','${username}','${email}','${password}') `;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      console.log("added new user");
+      res.redirect("/user");
+    });
+  } catch (err) {
+    res.send("some error occurred");
+  }
+});
+
+//delete user
+
+app.get("/user/:id/delete", (req, res) => {
+  let { id } = req.params;
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+      res.render("delete.ejs", { user });
+    });
+  } catch (err) {
+    res.send("some error with DB");
+  }
+});
+
+app.delete("/user/:id/", (req, res) => {
+  let { id } = req.params;
+  let { password } = req.body;
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      let user = result[0];
+
+      if (user.password != password) {
+        res.send("WRONG Password entered!");
+      } else {
+        let q2 = `DELETE FROM user WHERE id='${id}'`; //Query to Delete
+        connection.query(q2, (err, result) => {
+          if (err) throw err;
+          else {
+            console.log(result);
+            console.log("deleted!");
+            res.redirect("/user");
+          }
+        });
+      }
+    });
+  } catch (err) {
+    res.send("some error with DB");
+  }
+});
+
+
+app.listen("8080", () => {
   console.log("Server is listening to port 8080")
 })
 
